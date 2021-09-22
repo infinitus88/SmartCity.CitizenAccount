95aa04923c7581749a84e2509ad22674b9eae9c8
Rustam Minnikhanov <minnikhanovrustam@gmail.com>
2021-09-23 03:01:43 +0500

    Add Access-Control-Allow-Origin header to all axios requests

--- src/client/src/axios.js
+++ src/client/src/axios.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 const baseURL = 'https://smartcity-citizenaccount.azurewebsites.net'
 
 export default axios.create({
-  baseURL
+  baseURL,
   // You can add your headers here
-  // headers: { 'Access-Control-Allow-Origin': '*' }
+  headers: { 'Access-Control-Allow-Origin': '*' }
 })
