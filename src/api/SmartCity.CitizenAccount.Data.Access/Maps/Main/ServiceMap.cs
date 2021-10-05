5fd1f0c4fbaeca1282d44e5ccc84e87b14213a12
Rustam Minnikhanov <minnikhanovrustam@gmail.com>
2021-10-05 13:38:54 +0500

    Clean code

---
+++ src/api/SmartCity.CitizenAccount.Data.Access/Maps/Main/ServiceMap.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartCity.CitizenAccount.Data.Access.Maps.Common;
+using SmartCity.CitizenAccount.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartCity.CitizenAccount.Data.Access.Maps.Main
+{
+    public class ServiceMap : IMap
+    {
+        public void Visit(ModelBuilder builder)
+        {
+            var entity = builder.Entity<Service>();
+            entity.ToTable("Services").HasKey(x => x.Id);
+        }
+    }
+}
--- src/api/SmartCity.CitizenAccount.Data.Access/Maps/Main/UserMap.cs
+++ src/api/SmartCity.CitizenAccount.Data.Access/Maps/Main/UserMap.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SmartCity.CitizenAccount.Data.Access.Constants;
+using SmartCity.CitizenAccount.Data.Access.Helpers;
 using SmartCity.CitizenAccount.Data.Access.Maps.Common;
 using SmartCity.CitizenAccount.Data.Models;
 
@@ -8,9 +10,24 @@ namespace SmartCity.CitizenAccount.Data.Access.Maps.Main
     {
         public void Visit(ModelBuilder builder)
         {
-            builder.Entity<User>()
-                .ToTable("Users")
+            var entity = builder.Entity<User>();
+            entity.ToTable("Users")
                 .HasKey(x => x.Id);
+
+            entity.HasData(new User
+            {
+                Id = 1,
+                DisplayName = "Rustam Minnikhanov",
+                Email = "minnikhanovrustam@gmail.com",
+                Password = "123456".WithBCrypt(),
+                Role = "admin",
+                Status = Status.Active,
+                PhotoUrl = Images.DefaultAvatar,
+                CitizenId = null,
+                IsVerified = false,
+                About = "",
+                IsDeleted = false
+            });
         }
     }
 }
