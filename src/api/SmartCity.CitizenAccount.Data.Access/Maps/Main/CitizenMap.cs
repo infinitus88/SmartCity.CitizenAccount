4507c9730ce990d3e317c25b3322026a75ac8fb6
Rustam Minnikhanov <minnikhanovrustam@gmail.com>
2021-10-05 13:38:30 +0500

    Clean code

--- src/api/SmartCity.CitizenAccount.Data.Access/Maps/Main/CitizenMap.cs
+++ src/api/SmartCity.CitizenAccount.Data.Access/Maps/Main/CitizenMap.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SmartCity.CitizenAccount.Data.Access.Constants;
 using SmartCity.CitizenAccount.Data.Access.Maps.Common;
 using SmartCity.CitizenAccount.Data.Models;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 
 namespace SmartCity.CitizenAccount.Data.Access.Maps.Main
@@ -15,10 +17,20 @@ namespace SmartCity.CitizenAccount.Data.Access.Maps.Main
             entity.ToTable("Citizens").HasKey(x => x.Id);
             entity.Property(x => x.Id).HasDefaultValueSql("NEWID()");
 
-            entity
-                .Property(x => x.Balance)
+            entity.Property(x => x.Balance)
                 .HasColumnType("decimal(18,2)");
 
+            entity.HasData(new Citizen
+            {
+                Id = Guid.NewGuid().ToString().ToUpper(),
+                FullName = "Rustam Minnikhanov",
+                Balance = 0,
+                Gender = Gender.Male,
+                PhotoUrl = Images.DefaultAvatar,
+                DateOfBirth = DateTime.ParseExact("02/09/2001", "dd/MM/yyyy", CultureInfo.InvariantCulture),
+                IsDeleted = false,
+                RegistrationDate = DateTime.Now
+            });
         }
     }
 }
